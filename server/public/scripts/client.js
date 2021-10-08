540152dbfcc8154e7634d126c2f9028b646d12af
Amanda Zimmerman <amanda.zimms@gmail.com>
2021-10-07 20:03:29 -0500

    testing new remote origin

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,5 +11,27 @@ function setupClickListeners() {
 
 function addNewTask(){
   let taskString =  $( '#taskIn' ).val();
-  
+
+  let taskObj = {
+    task: taskString,
+    completed: false
+  }
+
+  $.ajax({
+    method: 'POST',
+    url: '/tasks',
+    data: taskObj
+
+  }).then( function (response) {
+    postAllTasks();
+    $( '#taskIn' ).val();
+
+  }).catch(function ( err ){
+    alert(`error adding task`);
+    console.log(err);
+  });
+}
+
+function postAllTasks(){
+  console.log('posting all tasks');
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const pool = require('./modules/pool');
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
-// ROUTES
-
-
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 });
+
+//ROUTES
+
+//POST
+app.post('/tasks', (req, res) => {
+  console.log('/post hit! req.query is:', req.query);
+
+  const queryString= ``;
+  let values = [];
+
+  pool.query(queryString, values).then((results)=>{
+    res.sendStatus(201);
+    
+  }).catch((err)=>{
+    console.log('error posting task:', err);
+    res.sendStatus(500);
+  })
+})
